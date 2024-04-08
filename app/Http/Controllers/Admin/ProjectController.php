7ebf19b259736b149dbad943daf1ece4c8d85109
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-04-08 09:05:56 +0200

    add edit content

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -60,7 +60,7 @@ class ProjectController extends Controller
      */
     public function edit(Project $project)
     {
-        //
+        return view('admin.projects.edit', compact('project'));
     }
 
     /**
@@ -71,7 +71,9 @@ class ProjectController extends Controller
      */
     public function update(Request $request, Project $project)
     {
-        //
+        $data = $request->all();
+        $project->update($data);
+        return redirect()->route('admin.projects.show', compact('project'))->with('message', 'Progetto modificato con successo');
     }
 
     /**
