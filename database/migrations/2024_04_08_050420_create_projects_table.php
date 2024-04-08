355f02fdd9d4f365ad64c49bc66c845bac11fb01
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-04-08 07:19:07 +0200

    Add project model and seeder

--- database/migrations/2024_04_08_050420_create_projects_table.php
+++ database/migrations/2024_04_08_050420_create_projects_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->string('author', 100);
+            $table->text('description')->nullable();
+            $table->string('project_link');
             $table->timestamps();
         });
     }
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Project;
 
 class ProjectSeeder extends Seeder
 {
@@ -14,6 +14,11 @@ class ProjectSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $project = new Project;
+        $project->author = 'Adriatik Berdufi';
+        $project->title = 'Max-Coach';
+        $project->description = 'Un Webapp per corsi online';
+        $project->project_link = 'https://github.com/Adriatik-Berdufi/proj-html-vuejs';
+        $project->save();
     }
 }
