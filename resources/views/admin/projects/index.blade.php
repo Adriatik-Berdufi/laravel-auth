4b0937aa2bb6913eb62d8e531234a4fdebed79e3
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-04-08 08:18:17 +0200

    add index content

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,8 +2,50 @@
 @section('title', 'Progetti')
 
 @section('content')
-    <h1>Index View</h1>
+    <div class="container">
+        <h1 class="my-3">Lista Progetti</h1>
 
+        <a href="{{ route('admin.projects.create') }}" class="btn btn-primary mb-3">Add new Project</a>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome del progetto</th>
+                    <th scope="col">Autore</th>
+                    <th scope="col">Link</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($projects as $project)
+                    <tr>
+                        <th scope="row">{{ $project->id }}</th>
+                        <td>{{ $project->title }}</td>
+                        <td>{{ $project->author }}</td>
+                        <td><a href="{{ $project->project_link }}" target="_blank">Go to the project</a></td>
+                        <td>
+                            <a href="{{ route('admin.projects.show', $project) }}" class="p-2 mx-2"><i
+                                    class="fa-solid fa-eye"></i></a>
+                            <a href="{{ route('admin.projects.edit', $project) }}" class="p-2 mx-2"><i
+                                    class="fa-solid fa-pen-to-square"></i></a>
+                            <button type="button" class="btn p-2 mx-2 modal-button" data-bs-toggle="modal"
+                                data-bs-target="#delete-project-{{ $project->id }}">
+                                <i class="fa-solid fa-circle-xmark" style="color: red;"></i>
+                            </button>
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="100%">Nessun risultato trovato</td>
+                    </tr>
+                @endforelse
+
+            </tbody>
+        </table>
+
+        {{ $projects->links() }}
+    </div>
 @endsection
 
 
