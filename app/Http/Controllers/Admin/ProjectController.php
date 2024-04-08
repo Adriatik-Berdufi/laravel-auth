5d820c3cb809555e302ceda3206d5445a9838261
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-04-08 08:27:56 +0200

    add create content

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -23,7 +23,7 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.projects.create');
     }
 
     /**
@@ -33,7 +33,14 @@ class ProjectController extends Controller
      */
     public function store(Request $request)
     {
-        //
+       
+
+        $data = $request->all();
+        $new_project = new Project;
+        $new_project->fill($data);
+        $new_project->save();
+
+        return redirect()->route('admin.projects.show', $new_project)->with('message', 'successfully created');
     }
 
     /**
